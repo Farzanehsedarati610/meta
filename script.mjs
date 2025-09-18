41b57edfe706f706f61ed02a9b070672fb9c0009
Farzanehsedarati610 <farzanehsedarati6@gmail.com>
2025-09-17 23:12:26 -0400

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -1,27 +1,54 @@
-const a(a){[084003997, 727762681039763]}
-  Const ab(ab){[041215663, 1298861419215]}; function a(a) {return a + 232000000;}console.log(a(232000000));
-const b(b){[084003997, 727762682039763]}
-  const ac(ac){[041215663, 1298861419215]}; function b(b) {return b + 232000000;}console.log(b(232000000));
-const c(c){[084003997, 727762683039763]}
-const ad(ad){[041215663, 1298861419215]}; function c(c) {return c + 232000000;}console.log(c(232000000));
-const d(d){[084003997, 727762684039763]}
-const ae(ae){[041215663, 1298861419215]}; function d(d) {return d + 232000000;}console.log(d(232000000));
-const e(e){[084003997, 727762685039763]}
-const af(af){[041215663, 1298861419215]}; function e(e) {return e + 232000000;}console.log(e(232000000));
-const f(f){[084003997, 727762686039763]}
-const ag(ag){[041215663, 1298861419215]}; function f(f) {return f + 232000000;}console.log(f(232000000));
-const g(g){[084003997, 72776268703976])}
-const ah(ah){[041215663, 1298861419215]}; function g(g) {return g + 232000000;}console.log(g(232000000));
-const h(h){[084003997, 727762688039763]}
-const ai(ai){[041215663, 1298861419215]}; function h(h) {return h + 232000000;}console.log(g(232000000));
-const i(i){[084003997, 727762689039763]}
-const aj(aj){[041215663, 1298861419215]}; function i(i) {return i + 232000000;}console.log(i(232000000));
-const j(j){[074900356, 727762683000000]}
-const ak(ak){[041215663, 1298861419215]}; function j(j) {return j + 232000000;}console.log(j(232000000));
-const k(k){[074900356, 7277626800039763]}
-const al(al){[041215663, 1298861419215]}; function k(k) {return k + 1250000;}console.log(k(1250000));
-const l(l){[074900356, 727762684000000]}
-const am(am){[041215663, 1298861419215]}; function l(l) {return l + 1250000;}console.log(l(1250000));
-push (a)(b)(c)(d)(e)(f)(g)(h)(i)(j)(k)(l);
+
+// Base sets
+const a = [084003997, 727762681039763];
+const b = [084003997, 727762682039763];
+const c = [084003997, 727762683039763];
+const d = [084003997, 727762684039763];
+const e = [084003997, 727762685039763];
+const f = [084003997, 727762686039763];
+const g = [084003997, 727762687039763];
+const h = [084003997, 727762688039763];
+const i = [084003997, 727762689039763];
+const j = [074900356, 727762683000000];
+const k = [074900356, 7277626800039763];
+const l = [074900356, 727762684000000];
+
+// Adjustment sets
+const ab = [041215663, 1298861419215];
+const ac = [041215663, 1298861419215];
+const ad = [041215663, 1298861419215];
+const ae = [041215663, 1298861419215];
+const af = [041215663, 1298861419215];
+const ag = [041215663, 1298861419215];
+const ah = [041215663, 1298861419215];
+const ai = [041215663, 1298861419215];
+const aj = [041215663, 1298861419215];
+const ak = [041215663, 1298861419215];
+const al = [041215663, 1298861419215];
+const am = [041215663, 1298861419215];
+
+// Combine function
+function combine(setA, setB) {
+  return setA.map((val, i) => val + setB[i]);
+}
+
+// Special override function
+function override() {
+  return 1250000;
+}
+
+// Log results
+console.log("a:", combine(a, ab));
+console.log("b:", combine(b, ac));
+console.log("c:", combine(c, ad));
+console.log("d:", combine(d, ae));
+console.log("e:", combine(e, af));
+console.log("f:", combine(f, ag));
+console.log("g:", combine(g, ah));
+console.log("h:", combine(h, ai));
+console.log("i:", combine(i, aj));
+console.log("j:", override());
+console.log("k:", override());
+console.log("l:", override());
 
 
